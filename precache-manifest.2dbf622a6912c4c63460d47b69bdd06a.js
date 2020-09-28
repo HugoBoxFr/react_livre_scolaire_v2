866265813a751a3c35420e3c57f48cf6d8c36cda
hugoprea <hugo.prea@coaxys.com>
2020-09-28 02:09:51 +0200

    Updates

---
+++ precache-manifest.2dbf622a6912c4c63460d47b69bdd06a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6808772e74c65b234930fb5598ad53d",
+    "url": "/react_livre_scolaire/index.html"
+  },
+  {
+    "revision": "15447d624ad827dfdbaa",
+    "url": "/react_livre_scolaire/static/css/main.6615a6d3.chunk.css"
+  },
+  {
+    "revision": "52ccf1c4caeb1a239dea",
+    "url": "/react_livre_scolaire/static/js/2.65c601a7.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/react_livre_scolaire/static/js/2.65c601a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15447d624ad827dfdbaa",
+    "url": "/react_livre_scolaire/static/js/main.65a2b426.chunk.js"
+  },
+  {
+    "revision": "7c7cea928b8159722baa",
+    "url": "/react_livre_scolaire/static/js/runtime-main.f2293793.js"
+  }
+]);
